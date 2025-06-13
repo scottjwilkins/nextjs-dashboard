1880c073b549c69c4619c51ab085f58c5deee475
Scott Wilkins <scottwilkins@Scotts-MacBook-Pro.local>
2025-06-13 10:43:48 -0700

    pretty2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: this skips _all_ ESLint checks at build time
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
